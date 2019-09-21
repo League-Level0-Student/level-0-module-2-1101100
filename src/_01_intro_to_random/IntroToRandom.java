cbd6b44b03efa129bb249adb796c178f557f45aa
League Student <league@iMac.attlocal.net>
2019-09-20 17:00:33 -0700

    Work on random

--- src/_01_intro_to_random/IntroToRandom.java
+++ src/_01_intro_to_random/IntroToRandom.java
@@ -14,8 +14,8 @@ public class IntroToRandom {
 		//3a. Create an object of the Random class, Hint: Random ran = new Random()
 		
 		//3b. Next, set the value of num, using .nextInt() to get a random number from the Random object
-		
-
+		//Random ran = new Random();
+		// num = ran.nextInt(((75-25)+1)+25);
 		//4. Now limit the value of num to be between 0 and 100
 
 		//5. Now limit the value of num to be between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
@@ -23,7 +23,12 @@ public class IntroToRandom {
 		//6. Challenge: Limit the value of num to be between -222 and 88
 
 		//1. Print out the value of num
-		
+		for (int i = 0; i < 20; i++) {
+			Random ran = new Random();
+			num = ran.nextInt((75-25)+1);
+			System.out.println(num+25);
+		}
+
 		//2. Run the program.  What number appears in the console?
 		//   Run it again. Is the number the same?
 	}
